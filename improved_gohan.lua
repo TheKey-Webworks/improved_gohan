11294b6b6e4d03e4347f5641faf8c033e3dc705f
TheKey-Webworks <163067791+TheKey-Webworks@users.noreply.github.com>
2024-10-25 20:08:10 -0300

    Create improved_gohan.lua

---
+++ improved_gohan.lua
@@ -0,0 +1,51 @@
+print("Improved V1.0")
+
+local p = game.Players.LocalPlayer
+local qg = game.workspace.Others.NPCs["Kid Nohag"]
+local events = game.ReplicatedStorage.Package.Events
+local pd = game.ReplicatedStorage.Datas[p.UserId]
+
+
+coroutine.wrap(function() 
+    while task.wait() do
+        for i = 1, 4 do
+            events.p:FireServer("Blacknwhite27", i)
+        end
+    end
+end)()
+
+while task.wait(0.1) do  -- Reduce la espera para mejorar la responsividad
+    
+    local clockTime = game.Lighting.ClockTime;
+    
+    if (clockTime > 0 and clockTime < 12) then
+        events.TP:InvokeServer("Earth")
+    end
+
+    if _G.rebirth then
+       events.reb:InvokeServer()
+    end
+
+    local _, qq = pcall(function() 
+        if pd.Quest.Value == "" then
+            -- Mueve el personaje hacia la posición de Kid Nohag
+            if p.Character and p.Character:FindFirstChild("HumanoidRootPart") then
+                p.Character.HumanoidRootPart.CFrame = qg.HumanoidRootPart.CFrame
+                print("Moviendo a Kid Nohag") -- Mensaje de depuración
+                events.Qaction:InvokeServer(qg)
+                task.wait(0.5) -- Esperar un poco para permitir que pd.Quest.Value se actualice
+            end
+        end
+
+        task.wait()
+
+        return pd.Quest.Value
+    end)
+
+    if (qq == "Kid Nohag") then
+        pcall(function() 
+            p.Character.HumanoidRootPart.CFrame = workspace.Living:FindFirstChild("Kid Nohag").HumanoidRootPart.CFrame
+            task.wait()
+        end)
+    end
+end
